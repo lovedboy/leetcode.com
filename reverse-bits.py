7918da37bd4ae29d7add89fc59c02270b6ee6e24
lovedboy <lovedboy.tk@qq.com>
2015-09-13 15:25:00 +0800

    debug

--- reverse-bits.py
+++ reverse-bits.py
@@ -6,13 +6,13 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        bit = bin(n)[2:]
-        bit = (32 - len(bit)) * '0' + bit
-        print bit
-        s = 1
-        sum = 0
-        for b in bit:
-            sum += int(b) * s
-            s <<= 1
-        return sum
+        num = 0
+        for x in range(0, 32):
+            num += (n & 1) * (1 << (31 -x) )
+            n >>= 1
+        return num
+
+
+
+
 
