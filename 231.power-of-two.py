fc2b4e95c4f6ab0494c330ede668e3ec68bcdfe3
lovedboy <lovedboy.tk@qq.com>
2015-09-13 14:25:57 +0800

    debug

---
+++ 231.power-of-two.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/power-of-two/
+
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n > 0 and n & (n -1 ) == 0
